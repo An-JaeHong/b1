3fcc886bb78c89bda480b8837192003dc12a30cb
jaehong <jaehongan56@gmail.com>
2024-11-25 18:17:40 +0900

    폐기후 수정

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -7,11 +7,11 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
+
         print("연연습");
-=======
+
         print("습연");
->>>>>>> 3155109ca330c44a6e0ba5a8ecbfa0bc83b889ff
+
     }
 
     // Update is called once per frame
