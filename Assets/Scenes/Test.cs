3155109ca330c44a6e0ba5a8ecbfa0bc83b889ff
SIRO\gmlwo <gmlwo990518@naver.com>
2024-11-25 18:06:24 +0900

    제가 수정한게 맞습니다

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("연습");
+        print("습연");
     }
 
     // Update is called once per frame
